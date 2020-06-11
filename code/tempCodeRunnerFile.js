2de84b1ac7244cfa9185b063e21cd960b666c969
TobinXu <tommybug@163.com>
2020-06-11 23:41:02 +0800

    commit on master

--- code/tempCodeRunnerFile.js
+++ code/tempCodeRunnerFile.js
@@ -1,1 +1,14 @@
-console.log(hw.next());
+function NumberFind(arr) {
+    var len = arr.length;
+    var res = -1;
+    if (len > 1) {
+        res = arr[0];
+        for (let i = 1; i < len; i++) {
+            res = res ^ arr[i];
+        }
+    }
+    return res;
+}
+
+arr = [1,1,2,3,2,3,4,4,6,6,7];
+console.log(NumberFind(arr));
